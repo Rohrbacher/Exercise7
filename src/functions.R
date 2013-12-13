879ec78b171183b1f78867a69597a70adf3771ca
Paul <paulmoritzwiegmann@me.com>
2013-12-13 15:45:38 +0100

    preconditions added to both functions

--- src/functions.R
+++ src/functions.R
@@ -1,12 +1,16 @@
 # student name: Paul Moritz Wiegmann
 # student ID: 54165pwi
 
+#PRECOND: (is.numeric(x), !is.matrix(x), !is.vector(x))
 addTwo <- function(x){
+  stopifnot(is.numeric(x), !is.matrix(x), !is.vector(x))
   x <- x+2
   return(x)
 }
 
+#PRECOND: (is.numeric(x), !is.matrix(x), !is.vector(x))
 addFour <- function(x){
+  stopifnot(is.numeric(x), !is.matrix(x), !is.vector(x))
   x <- x+4
   return(x)
 }
